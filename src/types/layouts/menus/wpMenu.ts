1728d3c4e9b459dd526575d6452d9397ac588e11
DigiwaymediaDenys <denys@digiwaymedia.com>
2024-09-23 16:53:17 +0200

    fixed error types menus

---
+++ src/types/layouts/menus/wpMenu.ts
@@ -0,0 +1,42 @@
+import { z } from "zod";
+import { NavLinkPropsSchema, NavListPropsSchema } from "../Nav";
+import { MenuSkeletonPropsSchema } from "./Skeleton";
+
+const wpNavLinkSchema = z.object({
+    title: z.string(),
+    url: z.string(),
+    isButton: z.boolean(),
+    isIcon: z.string(),
+    id: z.number(),
+});
+
+export const wpMenuPropsSchema = z.object({
+    menuId: z.number(),
+    skeleton: MenuSkeletonPropsSchema.optional(),
+    ...NavListPropsSchema.shape,
+    ...NavLinkPropsSchema.shape,
+});
+
+export const VerticalWpMenuPropsSchema = z.object({
+    menuId: z.number(),
+    className: z.string().optional(),
+    height: z.number().optional()
+});
+
+export const WpMenuResponseSchema = z.object({
+    id: z.number(),
+    items: z.array(z.object({
+        title: z.string(),
+        type: z.string(),
+        menu_order: z.number(),
+        parent_id: z.number(),
+        url: z.string(),
+        is_button: z.boolean(),
+        fa_icon_code: z.string()
+    })),
+    language_code: z.string(),
+})
+
+export type wpNavLink = z.infer<typeof wpNavLinkSchema>;
+export type wpMenuProps = z.infer<typeof wpMenuPropsSchema>;
+export type WpMenuResponseType = z.infer<typeof WpMenuResponseSchema>;
