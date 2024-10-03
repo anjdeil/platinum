2a3627231a8de067d580d012e17771b6c119582d
DigiwaymediaDenys <denys@digiwaymedia.com>
2024-10-03 14:30:07 +0200

    fixed sideList

--- src/components/Layouts/SideList/styles.ts
+++ src/components/Layouts/SideList/styles.ts
@@ -18,18 +18,37 @@ export const List = styled.ul<ListProps>`
 
 export const StyledListItem = styled.li<StyledListItemrops>`
     box-sizing: border-box;
+    /* background-color: transparent; */
     background-color: ${({ isActive, theme, hoverBackground = theme.colors.primary }) => isActive ? hoverBackground : 'transparent'};
+    font-size: ${({ fontSize = '16px' }) => fontSize};
+    line-height: ${({ lineHeight = '24px' }) => lineHeight};
+    font-weight: ${({ fontWeight = 400 }) => fontWeight};
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    gap: 8px;
     border-radius: ${({ borderRadius = '8px' }) => borderRadius};
     transition: all 0.2s ease;
     border: none;
     width: ${({ width = '100%' }) => width};
-    cursor: pointer;        
+    cursor: pointer;
+    text-transform: uppercase;
+
+    @media ${({ theme }) => theme.media.large} {
+        font-size: ${({ tabletFontSize = '14px' }) => tabletFontSize};
+        line-height: ${({ tabletLineHeight = '24px' }) => tabletLineHeight};
+    }
+
+    @media ${({ theme }) => theme.media.medium} {
+        font-size: ${({ mobFontSize = '14px' }) => mobFontSize};
+        line-height: ${({ mobLineHeight = '24px' }) => mobLineHeight};
+    }
 
     & a,
     & button {
         width: 100%;
         padding: 16px;
-        text-decoration: none;       
+        text-decoration: none;
         color: ${({ isActive, theme }) => isActive ? theme.colors.white : theme.colors.black};
         text-transform: uppercase;
         font-size: ${({ fontSize = '16px' }) => fontSize};
