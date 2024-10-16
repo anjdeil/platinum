a5aaabaec6645bc65fdca0e4ddb4f8f909a048fa
IliaDigiWay <168741673+IliaDigiWay@users.noreply.github.com>
2024-10-16 17:29:41 +0200

    Update index.ts

--- src/types/layouts/BottomMenu/index.ts
+++ src/types/layouts/BottomMenu/index.ts
@@ -2,6 +2,7 @@ import { z } from 'zod';
 
 export const BottomMenuWrapperPropsSchema = z.object({
     height: z.string().optional(),
+    paddingBlock: z.string().optional()
 });
 
 export type BottomMenuWrapperProps = z.infer<typeof BottomMenuWrapperPropsSchema>;
