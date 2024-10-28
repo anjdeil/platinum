80a0d484d35e0b5a8db980a5982b6212346cf039
IliaDigiWay <168741673+IliaDigiWay@users.noreply.github.com>
2024-10-28 18:01:12 +0100

    Update PriceFilter.tsx

--- src/components/shop/filtration/PriceFilter/PriceFilter.tsx
+++ src/components/shop/filtration/PriceFilter/PriceFilter.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { StyledInput } from "./styles";
 import { CustomInput } from "@/components/global/forms/CustomInput";
+import { CustomInputStyle, CustomInputWrapper, Input } from "@/components/global/forms/CustomInput/styles";
 
 interface PriceFilter
 {
@@ -14,10 +15,22 @@ export const PriceFilter: FC<PriceFilter> = ({ maxPrice, minPrice }) =>
 {
     return (
         <div>
-            {/* <StyledInput /> */}
-            <CustomInput
-                isRequire={false}
-            />
+            <div>
+                <CustomInputStyle
+                    as={'label'}
+                    isError={false}
+                    isTextArea={false}
+                    isCheckbox={false}
+                    isPhone={false}>
+                    <CustomInputWrapper>
+                        <Input
+                            as={'input'}
+                            type={'number'}
+                            isCheckbox={false}
+                            isError={false} />
+                    </CustomInputWrapper>
+                </CustomInputStyle>
+            </div>
             <p>zł</p>
         </div>
     )
