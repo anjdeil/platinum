de337cca79dd5d6f1f431cd8e2810838c83b49bf
camelCaserJs <evgen@digiwaymedia.com>
2024-11-06 13:20:46 +0100

    Search bar added

--- src/components/widgets/Header/Header.tsx
+++ src/components/widgets/Header/Header.tsx
@@ -1,35 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Nav from '../../menus/Nav/Nav';
-import SearchBar from '../../global/SearchBar/SearchBar';
-import { HeaderContainer, HeaderContent, HeaderIcons, HeaderNav, HeaderSearch, HeaderWrapper } from './styles';
+import { HeaderContainer, HeaderContent, HeaderIcons, HeaderNav, HeaderWrapper } from './styles';
 import AccountIcon from '@/components/global/icons/AccountIcon/AccountIcon';
 import CartIcon from '@/components/global/icons/CartIcon/CartIcon';
 import HeartIcon from '@/components/global/icons/HeartIcon/HeartIcon';
 import IconButton from '@/components/global/buttons/IconButton/IconButton';
+import FindIcon from '@/components/global/icons/FindIcon/FindIcon';
+import SearchBar from '@/components/global/SearchBar/SearchBar';
 
-const Header: React.FC = () =>
-{
+const Header: React.FC = () => {
+
+    const [displayedSearchBar, setDisplayedSearchBar] = useState(false);
+
     return (
         <HeaderWrapper>
             <HeaderContainer>
-                <HeaderNav>
-                    <Nav
-                        menuId={344}
-                        skeleton={{
-                            elements: 4,
-                            width: "80px",
-                            height: "24px",
-                            gap: '24px'
-                        }}
-                        texttransform='uppercase'
-                        justify='space-evenly'
-                    />
-                </HeaderNav>
+                {!displayedSearchBar ?
+                    <HeaderNav>
+                        <Nav
+                            menuId={344}
+                            skeleton={{
+                                elements: 4,
+                                width: "80px",
+                                height: "24px",
+                                gap: '24px'
+                            }}
+                            texttransform='uppercase'
+                            justify='space-evenly'
+                        />
+                    </HeaderNav> :
+                    <SearchBar onClose={() => setDisplayedSearchBar(false)} />
+                }
                 <HeaderContent>
-                    <HeaderSearch>
-                        <SearchBar />
-                    </HeaderSearch>
                     <HeaderIcons>
+                        {!displayedSearchBar &&
+                            <IconButton onClick={() => setDisplayedSearchBar(true)} IconComponent={FindIcon} />
+                        }
                         <IconButton href='/my-account' IconComponent={AccountIcon} />
                         <IconButton count={3} IconComponent={CartIcon} />
                         <IconButton count={2} IconComponent={HeartIcon} />
