93d01dcbb936e3c2c93b597b3238b95980c4033f
IliaDigiWay <168741673+IliaDigiWay@users.noreply.github.com>
2024-11-08 13:33:27 +0100

    Update [slug].tsx

--- src/pages/product/[slug].tsx
+++ src/pages/product/[slug].tsx
@@ -1,26 +1,61 @@
 import Breadcrumbs from "@/components/global/Breadcrumbs/Breadcrumbs";
 import ProductInfo from "@/components/pages/product/ProductInfo/ProductInfo";
+import { customRestApi } from "@/services/wpCustomApi";
 import { useGetProductQuery } from "@/store/rtk-queries/wpCustomApi";
 import { Container } from "@/styles/components";
 import { ProductType } from "@/types/components/shop/product/products";
 import { Box } from "@mui/material";
+import { GetServerSideProps, GetServerSidePropsContext } from "next";
 import Head from "next/head";
 import { useEffect } from "react";
 
-export default function ProductPage()
+export const getServerSideProps: GetServerSideProps = async (context: GetServerSidePropsContext) =>
 {
-    const { data } = useGetProductQuery({ slug: 'uv-eyelash-extension-kit' });
+    const { slug } = context.query;
+
+    try
+    {
+        if (typeof slug !== "string")
+        {
+            throw new Error("Invalid product slug");
+        }
+
+        const response = await customRestApi.get(`products/${slug}`);
+
+        if (!response.data || !response.data.data.item)
+        {
+            throw new Error("Product not found");
+        }
+
+        return {
+            props: {
+                product: response.data.data.item,
+            },
+        };
+    } catch (err)
+    {
+        console.error("Failed to fetch product data:", err);
+        return {
+            notFound: true,
+        };
+    }
+};
+
+export default function ProductPage({ product })
+{
+    // const { data } = useGetProductQuery({ slug: 'uv-eyelash-extension-kit' });
     // chocolate-platinum-20-lines-mix-cc-curl
-    const product: ProductType | undefined = data?.data?.item;
+    // const product: ProductType | undefined = data?.data?.item;
+
 
     useEffect(() =>
     {
-        if (data)
+        if (product)
         {
-            console.log(data);
+            console.log(product);
 
         }
-    }, [data])
+    }, [product])
 
     return (
         <>
