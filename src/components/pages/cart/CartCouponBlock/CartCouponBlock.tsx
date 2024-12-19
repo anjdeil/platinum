136d488a101ac08eb401875e6b6e881b4d41649c
AlexandraDDD <121177907+AlexandraDDD@users.noreply.github.com>
2024-12-19 17:54:09 +0300

    fix

--- src/components/pages/cart/CartCouponBlock/CartCouponBlock.tsx
+++ src/components/pages/cart/CartCouponBlock/CartCouponBlock.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react'
+import React, { FC, useState } from "react";
 import {
   CouponBlock,
   CouponButton,
@@ -6,75 +6,68 @@ import {
   CouponForm,
   CouponSuccess,
   CouponText,
-} from './style'
-import theme from '@/styles/theme'
-import { useForm } from 'react-hook-form'
-import { useListAllCouponsQuery } from '@/store/rtk-queries/wooCustomApi'
-import { useTranslations } from 'next-intl'
-import { useAppDispatch } from '@/store'
-import { addCoupon } from '@/store/slices/cartSlice'
-import { CustomFormInput } from '@/components/global/forms/CustomFormInput'
-import { useResponsive } from '@/hooks/useResponsive'
-import { MenuSkeleton } from '@/components/menus/MenuSkeleton'
-import { CartCouponBlockProps } from '@/types/pages/cart'
-import {
-  discountMapping,
-  userLoyalityStatusSchema,
-} from '@/types/store/rtk-queries/wpApi'
+} from "./style";
+import theme from "@/styles/theme";
+import { useForm } from "react-hook-form";
+import { useListAllCouponsQuery } from "@/store/rtk-queries/wooCustomApi";
+import { useTranslations } from "next-intl";
+import { useAppDispatch } from "@/store";
+import { addCoupon } from "@/store/slices/cartSlice";
+import { CustomFormInput } from "@/components/global/forms/CustomFormInput";
+import { useResponsive } from "@/hooks/useResponsive";
+import { MenuSkeleton } from "@/components/menus/MenuSkeleton";
+import { CartCouponBlockProps } from "@/types/pages/cart";
+import { discountMapping, userLoyalityStatusSchema } from "@/types/store/rtk-queries/wpApi";
 
-const CartCouponBlock: FC<CartCouponBlockProps> = ({
-  symbol,
-  auth,
-  userLoyalityStatus,
-}) => {
-  const { isMobile } = useResponsive()
-  const t = useTranslations('Cart')
-  const dispatch = useAppDispatch()
+const CartCouponBlock: FC<CartCouponBlockProps> = ({ symbol, auth, userLoyalityStatus }) => {
+  const { isMobile } = useResponsive();
+  const t = useTranslations("Cart");
+  const dispatch = useAppDispatch();
 
-  const isValidStatus = userLoyalityStatusSchema.safeParse(userLoyalityStatus)
-  const validStatus = isValidStatus.data
+  const isValidStatus = userLoyalityStatusSchema.safeParse(userLoyalityStatus);
+  const validStatus = isValidStatus.data;
 
   const {
     register,
     handleSubmit,
     formState: { errors },
     setValue,
-  } = useForm()
-  const { data: coupons, isLoading } = useListAllCouponsQuery()
+  } = useForm();
+  const { data: coupons, isLoading } = useListAllCouponsQuery();
 
-  const [couponState, setCouponState] = useState<'success' | 'error' | null>(null)
+  const [couponState, setCouponState] = useState<"success" | "error" | null>(null);
 
   const onSubmit = (data: any) => {
-    const isValidCoupon = coupons?.some((coupon) => coupon.code === data.couponCode)
+    const isValidCoupon = coupons?.some((coupon) => coupon.code === data.couponCode);
     if (isValidCoupon) {
-      dispatch(addCoupon({ couponCode: data.couponCode }))
-      setCouponState('success')
+      dispatch(addCoupon({ couponCode: data.couponCode }));
+      setCouponState("success");
     } else {
-      setCouponState('error')
+      setCouponState("error");
     }
-  }
+  };
 
   return (
     <CouponBlock>
       {!auth && (
         <CouponText uppercase marginBottom="8px">
           {/*  need to add diff  DISCOUNT */}
-          {t('LoginAnd')} <span>&nbsp;-3%&nbsp;</span> {t('ForOrders')} {t('Above')}{' '}
-          <span>&nbsp;500 {symbol}&nbsp;</span>,<span>&nbsp;5%&nbsp;</span> {t('Above')}{' '}
-          <span>&nbsp;1000{symbol}&nbsp;</span>, <span>&nbsp;-10%&nbsp;</span>{' '}
-          {t('Above')} <span>&nbsp;2000{symbol}&nbsp;</span>
+          {t("LoginAnd")} <span>&nbsp;-3%&nbsp;</span> {t("ForOrders")} {t("Above")}{" "}
+          <span>&nbsp;500 {symbol}&nbsp;</span>,<span>&nbsp;5%&nbsp;</span> {t("Above")}{" "}
+          <span>&nbsp;1000{symbol}&nbsp;</span>, <span>&nbsp;-10%&nbsp;</span> {t("Above")}{" "}
+          <span>&nbsp;2000{symbol}&nbsp;</span>
         </CouponText>
       )}
       {validStatus && (
         <CouponText uppercase marginBottom="8px">
-          {t('UserLoyalityStatus', { validStatus: validStatus })}
+          {t("UserLoyalityStatus", { validStatus: validStatus })}
 
           <span>&nbsp;{discountMapping[validStatus] as string} &nbsp;</span>
         </CouponText>
       )}
 
-      <CouponText uppercase>{t('CouponText')}</CouponText>
-      <CouponText>{t('ChooseCouponText')}</CouponText>
+      <CouponText uppercase>{t("CouponText")}</CouponText>
+      <CouponText>{t("ChooseCouponText")}</CouponText>
 
       {isLoading ? (
         <MenuSkeleton
@@ -89,7 +82,7 @@ const CartCouponBlock: FC<CartCouponBlockProps> = ({
         <>
           <CouponForm onSubmit={handleSubmit(onSubmit)}>
             <CustomFormInput
-              width={isMobile ? '100%' : 'auto'}
+              width={isMobile ? "100%" : "auto"}
               label={false}
               name="couponCode"
               register={register}
@@ -97,17 +90,17 @@ const CartCouponBlock: FC<CartCouponBlockProps> = ({
               inputTag="input"
               inputType="text"
               setValue={setValue}
-              placeholder={t('CouponInputPlaceholder')}
+              placeholder={t("CouponInputPlaceholder")}
               height="100%"
             />
-            <CouponButton type="submit">{t('CouponApplyBtn')}</CouponButton>
+            <CouponButton type="submit">{t("CouponApplyBtn")}</CouponButton>
           </CouponForm>
-          {couponState === 'error' && <CouponError>{t('ErrorCoupon')}</CouponError>}
-          {couponState === 'success' && <CouponSuccess>{t('CouponApply')}</CouponSuccess>}
+          {couponState === "error" && <CouponError>{t("ErrorCoupon")}</CouponError>}
+          {couponState === "success" && <CouponSuccess>{t("CouponApply")}</CouponSuccess>}
         </>
       )}
     </CouponBlock>
-  )
-}
+  );
+};
 
-export default CartCouponBlock
+export default CartCouponBlock;
--- src/types/store/rtk-queries/wpApi.ts
+++ src/types/store/rtk-queries/wpApi.ts
@@ -1,23 +1,23 @@
-import { z } from 'zod'
+import { z } from "zod";
 
-export const userLoyalityStatusSchema = z.enum(['silver', 'gold', 'platinum'])
-export type UserLoyalityStatusType = z.infer<typeof userLoyalityStatusSchema>
+export const userLoyalityStatusSchema = z.enum(["silver", "gold", "platinum"]);
+export type UserLoyalityStatusType = z.infer<typeof userLoyalityStatusSchema>;
 
 export const discountMapping: Record<UserLoyalityStatusType, string> = {
-  silver: '-3%',
-  gold: '-5%',
-  platinum: '-10%',
-}
+  silver: "-5%",
+  gold: "-10%",
+  platinum: "-15%",
+};
 
 export const WishlistItemSchema = z.object({
   product_id: z.number(),
   variation_id: z.number().optional(),
-})
+});
 
 const MetaSchema = z.object({
   loyalty: userLoyalityStatusSchema,
   wishlist: z.array(WishlistItemSchema),
-})
+});
 
 const WpUserSchema = z.object({
   avatar_urls: z.object({}),
@@ -30,7 +30,7 @@ const WpUserSchema = z.object({
   name: z.string(),
   slug: z.string(),
   url: z.string().url(),
-})
+});
 
-export type WpUserType = z.infer<typeof WpUserSchema>
-export type WishlistItem = z.infer<typeof WishlistItemSchema>
+export type WpUserType = z.infer<typeof WpUserSchema>;
+export type WishlistItem = z.infer<typeof WishlistItemSchema>;
