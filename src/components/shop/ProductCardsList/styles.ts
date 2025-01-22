e307f65c1b8fe05e772f499925f0dc401d7ac646
AlexandraDDD <121177907+AlexandraDDD@users.noreply.github.com>
2025-01-22 16:39:41 +0300

    done

--- src/components/shop/ProductCardsList/styles.ts
+++ src/components/shop/ProductCardsList/styles.ts
@@ -1,19 +1,26 @@
-import { StyledProductCardListProps } from "@/types/components/shop";
-import styled from "@emotion/styled";
+import { StyledProductCardListProps } from '@/types/components/shop';
+import styled from '@emotion/styled';
 
 export const StyledProductCardList = styled.div<StyledProductCardListProps>`
-    width: 100%;
-    display: grid;
-    justify-content: space-between;
-    gap: ${({ gap = "16px" }) => gap};
-    grid-template-columns: ${({ desktopColumns = 3 }) => `repeat(${desktopColumns}, 1fr)`};
+  width: 100%;
+  display: grid;
+  justify-content: space-between;
+  gap: ${({ gap = '16px' }) => gap};
+  grid-template-columns: ${({ desktopColumns = 3 }) =>
+    `repeat(${desktopColumns}, 1fr)`};
 
-    @media ${({ theme }) => theme.media.large} {        
-        grid-template-columns: ${({ tabletColumns = 4 }) => `repeat(${tabletColumns}, 1fr)`};
-    }
+  @media ${({ theme }) => theme.media.large} {
+    grid-template-columns: ${({ tabletColumns = 4 }) =>
+      `repeat(${tabletColumns}, 1fr)`};
+  }
+  @media ${({ theme }) => theme.media.mediumLarge} {
+    grid-template-columns: ${({ mintabletColumns = 3 }) =>
+      `repeat(${mintabletColumns}, 1fr)`};
+  }
 
-    @media ${({ theme }) => theme.media.medium} {   
-        grid-template-columns: ${({ mobileColumns = 2 }) => `repeat(${mobileColumns}, 1fr)`};     
-        gap: ${({ mobGap = "8px" }) => mobGap};
-    }
+  @media ${({ theme }) => theme.media.medium} {
+    grid-template-columns: ${({ mobileColumns = 2 }) =>
+      `repeat(${mobileColumns}, 1fr)`};
+    gap: ${({ mobGap = '8px' }) => mobGap};
+  }
 `;
