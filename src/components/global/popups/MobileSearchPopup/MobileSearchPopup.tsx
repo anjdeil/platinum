3a878fc8aeb45bd8786b78db151916c74b1b3896
AlexandraDDD <121177907+AlexandraDDD@users.noreply.github.com>
2025-02-05 18:38:32 +0300

    Update MobileSearchPopup.tsx

--- src/components/global/popups/MobileSearchPopup/MobileSearchPopup.tsx
+++ src/components/global/popups/MobileSearchPopup/MobileSearchPopup.tsx
@@ -1,56 +1,62 @@
-import { useSearchLogic } from "@/hooks/useSearchLogic";
-import { FC, useEffect, useRef } from "react";
-import SearchInputComponent from "../../SearchInputComponent/SearchInputComponent";
-import SearchResultsComponent from "../../SearchResultsComponent/SearchResultsComponent";
-import { InputWrapper, SearchContent, SearchWrapper, TopBarWrapper } from "./styles";
+import { useSearchLogic } from '@/hooks/useSearchLogic';
+import { FC, useEffect, useRef } from 'react';
+import SearchInputComponent from '../../SearchInputComponent/SearchInputComponent';
+import SearchResultsComponent from '../../SearchResultsComponent/SearchResultsComponent';
+import {
+  InputWrapper,
+  SearchContent,
+  SearchWrapper,
+  TopBarWrapper,
+} from './styles';
 
 interface MobileCategoriesMenuPropsType {
-    onClose: () => void
+  onClose: () => void;
 }
 
 const MobileSearchPopup: FC<MobileCategoriesMenuPropsType> = ({ onClose }) => {
-    const {
-      searchTerm,
-      setFocused,
-      handleInputChange,
-      isLoading,
-      products,
-      childParentCategories,
-      routeToProduct,
-      routeToCategory,
-    } = useSearchLogic();
+  const {
+    searchTerm,
+    setFocused,
+    handleInputChange,
+    isLoading,
+    products,
+    childParentCategories,
+    routeToProduct,
+    routeToCategory,
+  } = useSearchLogic();
 
-    const SearchInputRef = useRef<HTMLInputElement>(null);
+  const SearchInputRef = useRef<HTMLInputElement>(null);
 
-    useEffect(() => {
-        SearchInputRef?.current?.focus();
-    }, []);
+  useEffect(() => {
+    SearchInputRef?.current?.focus();
+  }, []);
 
-    return (
-        <SearchWrapper>
-            <TopBarWrapper>
-                <InputWrapper>
-                    <SearchInputComponent
-                        searchTerm={searchTerm}
-                        isLoading={isLoading}
-                        onChange={handleInputChange}
-                        onFocus={() => setFocused(true)}
-                        onBlur={() => setFocused(false)}
-                        onClose={onClose}
-                        inputRef={SearchInputRef}
-                    />
-                </InputWrapper>
-            </TopBarWrapper>
-            <SearchContent>
-                <SearchResultsComponent
-                    products={products || []}
-                    categories={childParentCategories || []}
-                    onCategorySelect={(slug) => routeToCategory(slug)}
-                    onProductSelect={(slug) => routeToProduct(slug)}
-                />
-            </SearchContent>
-        </SearchWrapper>
-    );
-}
+  return (
+    <SearchWrapper>
+      <TopBarWrapper>
+        <InputWrapper>
+          <SearchInputComponent
+            searchTerm={searchTerm}
+            isLoading={isLoading}
+            onChange={handleInputChange}
+            onFocus={() => setFocused(true)}
+            onBlur={() => setFocused(false)}
+            onClose={onClose}
+            inputRef={SearchInputRef}
+          />
+        </InputWrapper>
+      </TopBarWrapper>
+      <SearchContent>
+        <SearchResultsComponent
+          searchTerm={searchTerm}
+          products={products || []}
+          categories={childParentCategories || []}
+          onCategorySelect={slug => routeToCategory(slug)}
+          onProductSelect={slug => routeToProduct(slug)}
+        />
+      </SearchContent>
+    </SearchWrapper>
+  );
+};
 
 export default MobileSearchPopup;
