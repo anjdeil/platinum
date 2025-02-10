7efcb5c0734e6d1122798903c0838c5c879ad45b
DigiwaymediaDenys <denys@digiwaymedia.com>
2025-02-10 12:11:40 +0100

    add translation to search input

--- src/components/global/buttons/MobileSearchButton/MobileSearchButton.tsx
+++ src/components/global/buttons/MobileSearchButton/MobileSearchButton.tsx
@@ -1,23 +1,25 @@
 import { popupSet } from '@/store/slices/PopupSlice';
+import { useTranslations } from 'next-intl';
 import { useDispatch } from 'react-redux';
 import { ButtonStyled, IconStyled } from './styles';
 
-
 const MobileSearchButton = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    return (
-        <ButtonStyled onClick={() => dispatch(popupSet('mobile-search'))}>
-            Search
-            <IconStyled
-                src={'/assets/icons/search.svg'}
-                alt={'Search'}
-                width={24}
-                height={24}
-                unoptimized={true}
-            />
-        </ButtonStyled>
-    );
+  const t = useTranslations('Search');
+
+  return (
+    <ButtonStyled onClick={() => dispatch(popupSet('mobile-search'))}>
+      {t('Search')}
+      <IconStyled
+        src={'/assets/icons/search.svg'}
+        alt={'Search'}
+        width={24}
+        height={24}
+        unoptimized={true}
+      />
+    </ButtonStyled>
+  );
 };
 
 export default MobileSearchButton;
