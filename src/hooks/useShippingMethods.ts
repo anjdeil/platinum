c3306a5031370c3a8729712b1b52960352b0c6f7
camelCaserJs <snake_case@i.ua>
2025-03-21 17:35:02 +0100

    Optimized shipping methods loading

--- src/hooks/useShippingMethods.ts
+++ src/hooks/useShippingMethods.ts
@@ -1,63 +1,61 @@
-import { useGetShippingZonesQuery, useLazyGetShippingZoneLocationsQuery, useLazyGetShippingZoneMethodsQuery } from "@/store/rtk-queries/wooCustomApi";
-import { ShippingsType } from "@/types/pages/checkout";
-import { ShippingMethodType } from "@/types/services";
-import { useEffect, useState } from "react";
-
+import {
+  useGetShippingZonesQuery,
+  useLazyGetShippingZoneLocationsQuery,
+  useLazyGetShippingZoneMethodsQuery,
+} from '@/store/rtk-queries/wooCustomApi';
+import { ShippingMethodType } from '@/types/services';
+import { useEffect, useState } from 'react';
 type UseShippingMethodsResult = {
-    shippingMethods: ShippingMethodType[];
-    isLoading: boolean;
-    isError: boolean;
+  shippingMethods: ShippingMethodType[];
+  isLoading: boolean;
+  isError: boolean;
 };
 
 export default function useShippingMethods(countryCode?: string): UseShippingMethodsResult {
-    const { data: shippingZones, isLoading: isShippingZonesLoading, isError } = useGetShippingZonesQuery();
+  const { data: shippingZones, isLoading: isShippingZonesLoading, isError } = useGetShippingZonesQuery();
+  const [isShippingMethodsLoading, setIsShippingMethodsLoading] = useState(false);
 
-    const [getShippingMethods, { isLoading: ishippingMethodsLoading }] = useLazyGetShippingZoneMethodsQuery();
-    const [getShippingLocations, { isLoading: isShippingLocationsLoading }] = useLazyGetShippingZoneLocationsQuery();
+  const [getShippingMethods] = useLazyGetShippingZoneMethodsQuery();
+  const [getShippingLocations] = useLazyGetShippingZoneLocationsQuery();
 
-    const [shippings, setShippings] = useState<ShippingsType[]>([]);
-    const [currentShippingMethods, setCurrentShippingMethods] = useState<ShippingMethodType[]>([]);
+  const [currentShippingMethods, setCurrentShippingMethods] = useState<ShippingMethodType[]>([]);
 
+  useEffect(() => {
+    if (!shippingZones) return;
 
-    useEffect(() => {
-        const fetchShippingMethodsAndLocations = async () => {
-            if (!shippingZones?.length) return;
+    const getTargetShippingZoneId = async () => {
+      setIsShippingMethodsLoading(true);
+      let targetShippingZoneId: number = 0;
 
-            let updatedShippings = await Promise.all(
-                shippingZones.map(async (shippingZone) => {
-                    const { data: methods = [] } = await getShippingMethods(shippingZone.id);
-                    const { data: locations = [] } = await getShippingLocations(shippingZone.id);
-
-                    return { ...shippingZone, methods, locations };
-                })
-            );
-
-            updatedShippings = updatedShippings.filter(({ methods }) => methods.length);
-
-            setShippings(updatedShippings);
+      for (const shippingZone of shippingZones) {
+        const { data: locations = [] } = await getShippingLocations(shippingZone.id);
+        if (locations.some(({ code }) => countryCode === code)) {
+          targetShippingZoneId = shippingZone.id;
+          break;
         }
+      }
 
-        fetchShippingMethodsAndLocations()
-    }, [shippingZones]);
+      if (!targetShippingZoneId) {
+        setCurrentShippingMethods([]);
+        setIsShippingMethodsLoading(false);
+        return;
+      }
 
+      const { data: methods = [] } = await getShippingMethods(targetShippingZoneId);
+      setCurrentShippingMethods(methods);
+      setIsShippingMethodsLoading(false);
 
-    useEffect(() => {
-        if (!shippings?.length || !countryCode) return;
-
-        const currentShipping = shippings.find(({ locations }) =>
-            locations.some(({ code }) => countryCode === code)
-        );
-
-        const shippingMethods = currentShipping ? currentShipping.methods : [];
-
-        setCurrentShippingMethods(shippingMethods);
-
-
-    }, [shippings, countryCode]);
-
-    return {
-        shippingMethods: currentShippingMethods,
-        isLoading: isShippingZonesLoading || ishippingMethodsLoading || isShippingLocationsLoading,
-        isError
     };
+
+    getTargetShippingZoneId();
+  }, [shippingZones, countryCode]);
+
+  useEffect(() => {
+  }, [ isShippingZonesLoading])
+
+  return {
+    shippingMethods: currentShippingMethods,
+    isLoading: isShippingZonesLoading || isShippingMethodsLoading,
+    isError,
+  };
 }
