2b8c4d8dbab27cc5c96c3f081c885bbcc1bd7d14
catherine <katsiaryna@digiwaymedia.com>
2025-04-15 21:51:31 +0200

    fix: fixed changing languages in 404 and 500 pages

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,22 +1,45 @@
 import ErrorPage from '@/components/pages/404/ErrorPage';
 import { PageTitle } from '@/components/pages/pageTitle';
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { NextIntlClientProvider } from 'next-intl';
+import { FlexBox } from '@/styles/components';
+import { CircularProgress } from '@mui/material';
 
 const isLocaleRuOrUk = (locale: string | undefined) =>
   locale === 'ru' || locale === 'uk';
 
 export default function NotFoundPage() {
   const { locale } = useRouter();
+  const [messages, setMessages] = useState<Record<string, string> | null>(null);
+
   const buttonWidth = isLocaleRuOrUk(locale) ? '314px' : '242px';
 
+  useEffect(() => {
+    const loadMessages = async () => {
+      const msgs = (await import(`../translations/${locale}.json`)).default;
+      setMessages(msgs);
+    };
+
+    loadMessages();
+  }, [locale]);
+
+  if (!locale || !messages) {
+    return (
+      <FlexBox justifyContent="center" alignItems="center" margin="100px">
+        <CircularProgress />
+      </FlexBox>
+    );
+  }
+
   return (
-    <>
+    <NextIntlClientProvider locale={locale} messages={messages}>
       <PageTitle nameSpace={'NotFoundPage'} spaceKey={'notFound'} />
       <ErrorPage
         isNotFoundPage
         imageURL={`/assets/images/404.svg`}
         buttonWidth={buttonWidth}
       />
-    </>
+    </NextIntlClientProvider>
   );
 }
