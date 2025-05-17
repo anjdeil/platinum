3a20bfd4eb42c4f5cfaf45be97383ab291e56f01
catherine <katsiaryna@digiwaymedia.com>
2025-05-17 15:52:59 +0200

    added getInitialProps to change locale for Html

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,35 +1,51 @@
-import { Head, Html, Main, NextScript } from 'next/document';
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document';
 import Script from 'next/script';
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        <Script
-          id="cookieyes"
-          type="text/javascript"
-          src="https://cdn-cookieyes.com/client_data/4892edb3dd7351862d65495e/script.js"
-          strategy="afterInteractive"
-        />
 
-        {/* Google tag */}
-        <Script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-4E4L0MVK9Q"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps, locale: ctx.locale || 'pl' };
+  }
+
+  render() {
+    const { locale } = this.props as any;
+
+    return (
+      <Html lang={locale || 'pl'}>
+        <Head>
+          <Script
+            id="cookieyes"
+            type="text/javascript"
+            src="https://cdn-cookieyes.com/client_data/4892edb3dd7351862d65495e/script.js"
+            strategy="afterInteractive"
+          />
+          <Script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-4E4L0MVK9Q"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'G-4E4L0MVK9Q');
           `}
-        </Script>
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+          </Script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
