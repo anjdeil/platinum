66526a9bb87244ec382ae5d96013d2098ead26ff
catherine <katsiaryna@digiwaymedia.com>
2025-05-21 16:49:45 +0200

    added structuredData to the slug page

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -31,16 +31,7 @@ import {
   StyledHeroImage,
 } from '@/styles/blog/styles';
 import Head from 'next/head';
-import he from 'he';
-
-const getCleanText = (html: string) => {
-  if (!html) return '';
-  const decoded = he.decode(html);
-  const noComments = decoded.replace(/<!--[\s\S]*?-->/g, '');
-  const noTags = noComments.replace(/<\/?[^>]+(>|$)/g, '');
-  const trimmed = noTags.trim().replace(/\s+/g, ' ');
-  return trimmed;
-};
+import { getCleanText } from '@/utils/getCleanText';
 
 export const getServerSideProps: GetServerSideProps = async (
   context: GetServerSidePropsContext
