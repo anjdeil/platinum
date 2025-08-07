8b44482fa8adce2e43433b377dd3832f7e9c1992
DigiwaymediaDenys <denys@digiwaymedia.com>
2025-08-07 11:20:25 +0200

    fixed nip regex

--- src/utils/validation/index.ts
+++ src/utils/validation/index.ts
@@ -7,4 +7,4 @@ export const apartmentRegex = /^[\p{Script=Latin}0-9\s.'\/,\-]*$/u;
 export const postcodeRegex = /^[A-Z0-9\s-]{3,10}$/i;
 export const emailRegex = /^(?!['`])\s*[-+.'\w]+@[-.\w]+\.[-.\w]+\s*$/gm;
 export const phoneRegex = /^\+\d{1,3}(?:[ -]?\d{2,4})+$/;
-export const nipRegex = /^[A-Z]{2}[A-Z0-9]{6,14}$/i;
+export const nipRegex = /^[A-Z0-9]{6,14}$/i;
