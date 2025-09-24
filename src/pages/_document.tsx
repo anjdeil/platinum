2ae8a454e6f2b49f892481098b485f3abae1baf5
maksymDigiway <maksym@digiway.dev>
2025-09-24 15:36:18 +0300

    test speed

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ import Document, {
   Main,
   NextScript,
 } from 'next/document';
-// import Script from 'next/script';
+import Script from 'next/script';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -19,37 +19,38 @@ class MyDocument extends Document {
     return (
       <Html lang={locale || 'pl'}>
         <Head>
-          {/*<Script*/}
-          {/*  id="cookieyes"*/}
-          {/*  type="text/javascript"*/}
-          {/*  src="https://cdn-cookieyes.com/client_data/4892edb3dd7351862d65495e/script.js"*/}
-          {/*  strategy="afterInteractive"*/}
-          {/*/>*/}
+          <Script
+            id="cookieyes"
+            type="text/javascript"
+            src="https://cdn-cookieyes.com/client_data/4892edb3dd7351862d65495e/script.js"
+            strategy="afterInteractive"
+            async
+          />
           {/* GTM */}
-          {/*<Script id="gtm" strategy="afterInteractive">*/}
-          {/*  {`*/}
-          {/*    (function(w,d,s,l,i){w[l]=w[l]||[];*/}
-          {/*      w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});*/}
-          {/*      var f=d.getElementsByTagName(s)[0],*/}
-          {/*      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';*/}
-          {/*      j.async=true;*/}
-          {/*      j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;*/}
-          {/*      f.parentNode.insertBefore(j,f);*/}
-          {/*    })(window,document,'script','dataLayer','GTM-NLKVWGD8');*/}
-          {/*  `}*/}
-          {/*</Script>*/}
+          <Script id="gtm" strategy="afterInteractive" async>
+            {`
+              (function(w,d,s,l,i){w[l]=w[l]||[];
+                w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+                j.async=true;
+                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NLKVWGD8');
+            `}
+          </Script>
         </Head>
         <body>
           {/* GTM noscript iframe */}
-          {/*<noscript>*/}
-          {/*  <iframe*/}
-          {/*    title="Google Tag Manager"*/}
-          {/*    src="https://www.googletagmanager.com/ns.html?id=GTM-NLKVWGD8"*/}
-          {/*    height="0"*/}
-          {/*    width="0"*/}
-          {/*    style={{ display: 'none', visibility: 'hidden' }}*/}
-          {/*  ></iframe>*/}
-          {/*</noscript>*/}
+          <noscript>
+            <iframe
+              title="Google Tag Manager"
+              src="https://www.googletagmanager.com/ns.html?id=GTM-NLKVWGD8"
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            ></iframe>
+          </noscript>
           <Main />
           <NextScript />
         </body>
